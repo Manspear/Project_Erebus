5f6703fe3d7dc9c1f7cc3d05eed3eb33f2f273bf
Fanny94 <funnei@hotmail.com>
2016-11-14 13:39:11 +0100

    created struct for particles.

--- LevelEditor/ParticleEditor.h
+++ LevelEditor/ParticleEditor.h
@@ -1,12 +1,25 @@
 #pragma once
 #include<AntTweakBar.h>
 
+
+struct Particle
+{
+	float pos[3];
+	float duration;
+	float speed;
+	float angle;
+	float color[3];
+};
+
 class ParticleEditor
 {
 private:
 	TwBar *myBar;
+	Particle* particle;
+
 public:
 	ParticleEditor();
 
 	~ParticleEditor();
+
 };
