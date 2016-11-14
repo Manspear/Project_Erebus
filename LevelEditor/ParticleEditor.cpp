d193aeb905f1b2573fc814b40e97563b4d9e26e9
Fanny94 <funnei@hotmail.com>
2016-11-14 16:17:29 +0100

    Did some small changes

--- LevelEditor/ParticleEditor.cpp
+++ LevelEditor/ParticleEditor.cpp
@@ -13,7 +13,7 @@ void ParticleEditor::particleEditorBar()
 	TwBar *particleBar;
 
 	particleBar = TwNewBar("Particle Editor Bar");
-	Particle akfh;
+	
 
 	TwAddVarRW(particleBar, "Position", TW_TYPE_DIR3F, &akfh.pos, " label='Pos'");
 	TwAddVarRW(particleBar, "Duration", TW_TYPE_FLOAT, &akfh.duration, " label='Duration'");
@@ -21,3 +21,11 @@ void ParticleEditor::particleEditorBar()
 	TwAddVarRW(particleBar, "Angle", TW_TYPE_FLOAT, &akfh.angle, " label='Angle'");
 	TwAddVarRW(particleBar, "Color", TW_TYPE_COLOR3F, &akfh.color, " label='Color'");
 }
+
+void ParticleEditor::createParticles()
+{
+
+
+
+}
+
