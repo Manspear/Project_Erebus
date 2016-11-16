4927137e222c5141c777ce0ba0c4b089499192d9
Taccoa <Mattias_M_93@hotmail.com>
2016-11-16 10:34:59 +0100

    Revert "include stuff"
    This reverts commit 2d2f4e1faa7c60b2acddd2687c7eb1a8b9cd40e5.

--- LevelEditor/ParticleEditor.h
+++ LevelEditor/ParticleEditor.h
@@ -1,8 +1,8 @@
 #pragma once
 #include<AntTweakBar.h>
+#include<GLFW\glfw3.h>
+#include<glm\vec3.hpp>
 #include"Particles.h"
-//#include<GLFW\glfw3.h>
-//#include<glm\vec3.hpp>
 
 class ParticleEditor 
 {
