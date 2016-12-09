4b84819133d61112978367d8b4fc472f5624859c
Jaddori <ohlsson.niclas@gmail.com>
2016-12-09 08:59:07 +0100

    Added collision to heightmap.

--- Erebus/Scripts/projectile.lua
+++ Erebus/Scripts/projectile.lua
@@ -3,6 +3,7 @@ projectile.transformID = Transform.Bind()
 projectile.position = {x=0, y=0, z=0}
 projectile.direction = {x=0, y=0, z=0}
 projectile.speed = 100
+projectile.alive = false
 
 local projectileModel = Assets.LoadModel( "Models/molerat.model" )
 Gear.AddModelInstance(projectileModel, projectile.transformID)
@@ -10,6 +11,7 @@ Gear.AddModelInstance(projectileModel, projectile.transformID)
 function projectile:Cast(position, direction)
 	self.position = position
 	self.direction = direction
+	self.alive = true
 	Transform.SetPosition(self.transformID, self.position)
 end
 
@@ -19,8 +21,24 @@ function projectile:AddPosition(dt)
 	self.position.z = self.position.z + ( self.direction.z * self.speed ) * dt
 end
 
+function projectile:Kill()
+	self.alive = false
+	self.position.x = 0
+	self.position.y = 0
+	self.position.z = 0
+	Transform.SetPosition(self.transformID, self.position)
+end
+
 function projectile:Update(dt)
+	if not self.alive then return end
+
 	self:AddPosition(dt)
+
+	local height = heightmap:GetHeight(self.position.x, self.position.z)
+	if self.position.y <= height then
+		self:Kill()
+	end
+
 	Transform.SetPosition(self.transformID, self.position)
 end
 
