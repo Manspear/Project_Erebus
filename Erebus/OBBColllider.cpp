57944ae296c95b5fbab7371f994de044af23dcd5
Elias <elias.frank@gmail.com>
2016-12-15 09:38:20 +0100

    Created OBBColllider

---
+++ Erebus/OBBColllider.cpp
@@ -0,0 +1,37 @@
+#include "OBBColllider.h"
+
+
+
+OBBColllider::OBBColllider()
+{
+}
+
+
+OBBColllider::~OBBColllider()
+{
+}
+
+unsigned int OBBColllider::getID() const
+{
+	return this->ID;
+}
+
+unsigned int OBBColllider::getIDTransform() const
+{
+	return this->IDTransform;
+}
+
+std::vector<unsigned int>* OBBColllider::getIDCollisionsRef()
+{
+	return &this->IDCollisions;
+}
+
+void OBBColllider::insertCollisionID(unsigned int collisionID)
+{
+	this->IDCollisions.push_back(collisionID);
+}
+
+void OBBColllider::clearCollisionIDs()
+{
+	this->IDCollisions.clear();
+}
---
+++ Erebus/OBBColllider.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "HitBox.h"
+class OBBColllider : public HitBox
+{
+public:
+	OBBColllider();
+	~OBBColllider();
+
+	//Overrides
+	virtual unsigned int getID() const override; // copy elision makes returning values fast? RVO - NRVO
+	virtual unsigned int getIDTransform() const override;
+	virtual std::vector<unsigned int>* getIDCollisionsRef() override;
+	virtual void insertCollisionID(unsigned int collisionID) override;
+	virtual void clearCollisionIDs() override;
+	
+private:
+	glm::vec3 pos;
+	glm::vec3 xAxis, yAxis, zAxis;
+	float xHalfLength, yHalfLength, zHalfLength;
+};
+
