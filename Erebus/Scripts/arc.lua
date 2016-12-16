1f8bce9440a52c52b6d624c9c832a76dc5815129
koolingtuffing <adam@fredrikson.net>
2016-12-16 13:59:17 +0100

    Nu exploderar arc i en radius

--- Erebus/Scripts/arc.lua
+++ Erebus/Scripts/arc.lua
@@ -4,16 +4,18 @@ local ARC_GRAVITY = 49.1
 function CreateArc()
 	local arc = dofile( "Scripts/basespell.lua" )
 	print(arc.BaseUpdate)
-	arc.speed = 50
-	arc.upSpeed = 50
+	arc.speed = 40
+	arc.upSpeed = 30
 	arc.currentUpSpeed = 0
 	arc.lifeTime = ARC_LIFETIME
-
+	arc.exploding = false
 	arc.damage = 5
+	arc.explodeTime = 1
 	local arcModel = Assets.LoadModel( "Models/testGuy.model" )
 	arc.sphereCollider = SphereCollider.Create(arc.transformID)
 	CollisionHandler.AddSphere(arc.sphereCollider)
-
+	SphereCollider.SetRadius(arc.sphereCollider, 5)
+	local fireball = createFireball()
 
 	Gear.AddStaticInstance(arcModel, arc.transformID)
 
@@ -21,45 +23,56 @@ function CreateArc()
 		self.position = Transform.GetPosition(player.transformID)
 		self.velocity = Transform.GetLookAt(player.transformID)
 		self.velocity.y = self.upSpeed
-
 		self.velocity.x = self.velocity.x * 50
 		self.velocity.z = self.velocity.z * 50
-
 		self.alive = true
 		self.lifeTime = ARC_LIFETIME
 		self.currentUpSpeed = self.upSpeed
+		arc.exploding = false
 		Transform.SetPosition(self.transformID, self.position)
+		fireball.cast()
 	end
 
 
 	function arc:Update(dt)
 		self.velocity.y = self.velocity.y - self.speed*dt
-
+		fireball.update(self.position.x, self.position.y, self.position.z)
 		local height = heightmap:GetHeight(self.position.x, self.position.z)
 		if self.position.y <= height then
-			self:Kill()
+			fireball.die(self.position.x, self.position.y, self.position.z)
+			arc.exploding = true
+			--self:Kill()
 		end
 
 		self.lifeTime = self.lifeTime - dt
 		if self.lifeTime<=0 then
-			self:Kill()
+			fireball.die(self.position.x, self.position.y, self.position.z)
+			arc.exploding = true
+			--self:Kill()
 		end
 		
-		local collisionIDs = self.sphereCollider:GetCollisionIDs()
-		for curID = 1, #collisionIDs do
-			for curEnemy=1, MAX_ENEMIES do
-				if collisionIDs[curID] == enemies[curEnemy].sphereCollider:GetID() then
-					self:Kill()
+		if arc.exploding == true then
+			local collisionIDs = self.sphereCollider:GetCollisionIDs()
+			for curID = 1, #collisionIDs do
+				for curEnemy=1, MAX_ENEMIES do
+					if collisionIDs[curID] == enemies[curEnemy].sphereCollider:GetID() then
+						print ("Träff för fan")
+						enemies[curEnemy]:Hurt(self.damage)
+						--self:Kill()						
+					end
 
-					enemies[curEnemy]:Hurt(self.damage)
+					--if not self.alive then break end
 				end
 
-				if not self.alive then break end
+				--if not self.alive then break end
 			end
-
-			if not self.alive then break end
 		end
-
+		if arc.exploding == true then
+			arc.explodeTime = arc.explodeTime - dt
+			if arc.explodeTime < 0.0 then
+				self:Kill()
+			end
+		end
 	end
 
 	return arc
--- Erebus/Scripts/particle.lua
+++ Erebus/Scripts/particle.lua
@@ -1,11 +1,11 @@
 function createFireball()
 	--Args = Antal partiklar, livstid, hastighet, utskjut/sekund, antal/utskjut
 	local fireball = {}
-	fireball.fly = Particle.Bind(50, 1, 2, 10, 5)
+	fireball.fly = Particle.Bind(50, 1, 2, 50, 1)
 	fireball.exploda = Particle.Bind(500, 1, 5, 1, 500)
 	Particle.SetColor(fireball.fly, 0.2, 0.5, 0.7)
 	Particle.SetColor(fireball.exploda, 0.85, 0.21, 0.11)
-	oneMoreTime = true
+
 	function fireball.cast()
 		Particle.SetAlive(fireball.fly)
 	end
