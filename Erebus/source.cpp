4d3ebe8c92980662478ad0444cc11fb2f031aae7
Dooby <bloom.filip@gmail.com>
2017-01-04 10:31:47 +0100

    removed debug code

--- Erebus/source.cpp
+++ Erebus/source.cpp
@@ -143,8 +143,6 @@ int main()
 		std::string fps = "FPS: " + std::to_string(counter.getFPS());
 		engine.print(fps, 0.0f, 0.0f, 0.9f);
 
-		engine.showImage(glm::vec2(100.f, 100.f), 100, 100, particlesTexture);
-
 		window.update();
 
 		engine.draw(&camera);
