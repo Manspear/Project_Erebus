7306ef4a552bb6becdde8b8e8fc0daf4c36e1cc8
koolingtuffing <adam@fredrikson.net>
2017-01-24 16:08:43 +0100

    SUNRAY

--- Erebus/Scripts/sunRay.lua
+++ Erebus/Scripts/sunRay.lua
@@ -20,43 +20,52 @@ function CreateSunRay()
 	sunRay.owner = {}
 	sunRay.moveImpairment = 0.5
 	sunRay.cameraSlow = 2.0
+	sunRay.cooldown = 0.0
+	sunRay.Charge = BaseCharge
+	sunRay.ChargeCast = BaseChargeCast
 
 	local model = Assets.LoadModel( "Models/sunRay.model" )
 	Gear.AddStaticInstance(model, sunRay.type.transformID)
 
 	function sunRay:Update(dt)
-		hits = self.type:Update(dt)
-		self.particles.update(self.type.position.x, self.type.position.y, self.type.position.z)
-		tempDir = Transform.GetRotation(player.tranformID)
-		Transform.SetRotation(self.type.transformID, Transform.GetRotation(player.tranformID))
-		Transform.SetLookAt(self.type.transformID, Transform.GetLookAt(player.tranformID))
-		for index = 1, #hits do
-			if hits[index].Hurt then	
-				if self.effectFlag then
-					table.insert(hits[index].effects, self.effect())
+		if self.alive then
+			hits = self.type:Update(dt)
+			self.particles.update(self.type.position.x, self.type.position.y, self.type.position.z)
+			tempDir = Transform.GetRotation(player.tranformID)
+			Transform.SetRotation(self.type.transformID, Transform.GetRotation(player.tranformID))
+			Transform.SetLookAt(self.type.transformID, Transform.GetLookAt(player.tranformID))
+			for index = 1, #hits do
+				if hits[index].Hurt then	
+					if self.effectFlag then
+						table.insert(hits[index].effects, self.effect())
+					end
+					hits[index]:Hurt(self.damage)
 				end
-				hits[index]:Hurt(self.damage)
+			end
+			self.type:Shoot(Transform.GetPosition(player.transformID), Camera.GetDirection(), 0)
+			self.lifeTime = self.lifeTime - dt
+			if self.lifeTime < 0 then 
+				self.particles.die(self.type.position) 
+				self:Kill() 
 			end
 		end
-		self.type:Shoot(Transform.GetPosition(player.transformID), Camera.GetDirection(), 0)
-		self.lifeTime = self.lifeTime - dt
-		if self.lifeTime < 0 then 
-			self.particles.die(self.type.position) 
-			self:Kill() 
-		end
+		self.cooldown = self.cooldown - dt;
 	end
 	
 	function sunRay:Cast(chargetime, effects)
-		self.particles.cast()
-		Erebus.CameraSensitivity(self.cameraSlow)
-		chargetime = math.min(chargetime, SUNRAY_MAX_CHARGETIME)
-		player.moveSpeed = player.moveSpeed * self.moveImpairment 
-		self.type:Shoot(Transform.GetPosition(player.transformID), Camera.GetDirection(), 0)
-		self.alive = true
-		self.lifeTime = SUNRAY_DURATION 
-		self.effectFlag = effects
-		self.damage = (chargetime/SUNRAY_MAX_CHARGETIME) * SUNRAY_DAMAGE
-		self.chargedTime = 0
+		if (self.cooldown < 0.0) then
+			self.particles.cast()
+			Erebus.CameraSensitivity(self.cameraSlow)
+			chargetime = math.min(chargetime, SUNRAY_MAX_CHARGETIME)
+			player.moveSpeed = player.moveSpeed * self.moveImpairment 
+			self.type:Shoot(Transform.GetPosition(player.transformID), Camera.GetDirection(), 0)
+			self.alive = true
+			self.lifeTime = SUNRAY_DURATION 
+			self.effectFlag = effects
+			self.damage = (chargetime/SUNRAY_MAX_CHARGETIME) * SUNRAY_DAMAGE
+			self.chargedTime = 0
+			self.cooldown = SUNRAY_COOLDOWN
+		end
 	end
 
 	function sunRay:Kill()
