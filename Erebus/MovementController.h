6405b42b6188154b9901e661e78bd966377e39f7
Jaddori <ohlsson.niclas@gmail.com>
2017-01-30 10:34:49 +0100

    Added movement controller.

---
+++ Erebus/MovementController.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CollisionHandler.h"
+
+class MovementController
+{
+public:
+	MovementController();
+	~MovementController();
+
+	void move( glm::vec3 distance );
+	void update();
+
+	void setHitbox( HitBox* hitbox );
+	void setTransform( Transform* transform );
+	void setCollisionLayer( CollisionLayers* layer );
+
+private:
+	HitBox* hitbox;
+	Transform* transform;
+	glm::vec3 movement;
+	CollisionLayers* collisionLayer;
+};
