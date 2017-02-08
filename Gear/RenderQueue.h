64023125f3ea1a959ef9cfb73d11a35d5c829c2e
Dooby <bloom.filip@gmail.com>
2017-02-08 14:58:50 +0100

    added VAO

--- Gear/RenderQueue.h
+++ Gear/RenderQueue.h
@@ -36,14 +36,37 @@ struct ModelInstance
 	Importer::ModelAsset* asset;
 	std::vector<int> worldIndices;
 	GLuint instanceVBO = 0;
+	GLuint instanceVAO = 0;
 
 	inline void allocateBuffer()
 	{
-		if (instanceVBO == 0)
+		if ((instanceVBO | instanceVAO) == 0)
+		{
 			glGenBuffers(1, &instanceVBO);
+			glGenVertexArrays(1, &instanceVAO);
+		}
+		
+		glBindVertexArray(instanceVAO);
 		glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::mat4) * worldIndices.size(), NULL, GL_STREAM_DRAW);
+
+		glEnableVertexAttribArray(4);
+		glEnableVertexAttribArray(5);
+		glEnableVertexAttribArray(6);
+		glEnableVertexAttribArray(7);
+
+		glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4) * 4, 0);
+		glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4) * 4, (void*)(sizeof(glm::vec4)));
+		glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4) * 4, (void*)(sizeof(glm::vec4) * 2));
+		glVertexAttribPointer(7, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4) * 4, (void*)(sizeof(glm::vec4) * 3));
+
+		glVertexAttribDivisor(4, 1);
+		glVertexAttribDivisor(5, 1);
+		glVertexAttribDivisor(6, 1);
+		glVertexAttribDivisor(7, 1);
+
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
 	}
 };
 struct AnimatedInstance
