6795cc96c433fcaad7641a12c49f52fe1517ec4f
drettnew <drettnew@gmail.com>
2017-02-09 10:44:21 +0100

    more UI and a setting file

--- Erebus/Scripts/spellbook.lua
+++ Erebus/Scripts/spellbook.lua
@@ -30,7 +30,11 @@ end
 
 function EnterSpellbook()
 	--Gear.QueueModels(false)
-	CollisionHandler.Disable()
+		if SETTING_DEBUG then 
+		CollisionHandler.Enable()
+	else
+		CollisionHandler.Disable()
+	end
 	Gear.CursorVisible(true)
 end
 
