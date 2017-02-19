593404754e4a5084c0fb088c19db87f31a6e09b1
koolingtuffing <adam@fredrikson.net>
2017-02-19 13:30:56 +0100

    Knockback krockar med väggar, fett OP

--- Erebus/Scripts/knockbackEffect.lua
+++ Erebus/Scripts/knockbackEffect.lua
@@ -1,20 +1,24 @@
 KNOCKBACK_SPEED = 2
-function CreateKnockbackEffect(entity, power)
+function CreateKnockbackEffect(owner, power)
 	local effect = {}
-	effect.owner = entity
+	effect.owner = owner
 	effect.notDone = true
 	effect.power = power or KNOCKBACK_SPEED
 	effect.direction = {x = 0, y = 0, z = 0}
 	effect.fallSpeed = 0
 	effect.position = {x = 0, y = 0, z = 0}
 	effect.damage = 0
-
+	effect.wallHit = false
+	effect.collisionID = -1
+	effect.YValue = 0
 	function effect:Apply(entity)
 		self.position = Transform.GetPosition(entity.transformID)
+		self.YValue = self.position.y
 		self.direction = vec3sub(self.position, self.owner.position)
 		self.direction.y =  1
 		self.fallSpeed = 3
 		self.damage = 1
+		self.collisionID = entity.sphereCollider:GetID()
 		if entity.SetState then
 			entity.SetState(entity,"DeadState")
 		end
@@ -27,12 +31,10 @@ function CreateKnockbackEffect(entity, power)
 	end
 
 	function effect:Update(entity, dt) --return false if you want the enemy to remove the effect from its effect list
-		self.position.x = self.position.x + self.direction.x * dt * self.power
-		self.position.y = self.position.y + self.direction.y * dt * self.power
-		self.position.z = self.position.z + self.direction.z * dt * self.power
-
-		Transform.SetPosition(entity.transformID, self.position)
-		self.direction.y = self.direction.y - self.fallSpeed * dt
+		self.position = Transform.GetPosition(entity.transformID)
+		self.YValue = self.YValue+ self.direction.y * dt * self.power
+		self.position.y = self.YValue
+		self.direction.y = self.direction.y - self.fallSpeed * dt		
 		self.damage = self.damage + 2 * dt
 		local hm = GetHeightmap(self.position)		
 		if hm then
@@ -42,6 +44,26 @@ function CreateKnockbackEffect(entity, power)
 			end
 		end
 
+		if not self.wallHit then 
+			self.position.x = self.position.x + self.direction.x * dt * self.power
+			self.position.z = self.position.z + self.direction.z * dt * self.power
+			
+			local collisionIDs = entity.sphereCollider:GetCollisionIDs()	
+			local walls = CollisionHandler.GetIDsFromLayer(3)
+			local realWalls = {}
+			for	curID = 1, #collisionIDs do
+				for i, v in pairs(walls) do
+					realWalls[i] = v
+				end		
+				for curWall = 1, #realWalls do
+					if collisionIDs[curID] == realWalls[curWall] then
+						self.wallHit = true
+						print("Tjena")
+					end
+				end
+			end
+		end
+		Transform.SetPosition(entity.transformID, self.position)	
 		return self.notDone
 	end
 	return effect
