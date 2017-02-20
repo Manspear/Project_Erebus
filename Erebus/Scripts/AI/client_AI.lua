cd1f391584da4308ecebf576160d33d2dbfe0c9b
Micadurp <neoswed@hotmail.com>
2017-02-20 13:42:25 +0100

    Changed functionparameter to be more clear

--- Erebus/Scripts/AI/client_AI.lua
+++ Erebus/Scripts/AI/client_AI.lua
@@ -3,36 +3,36 @@ local baseReturn = {}
 clientAIState = {idleState = {}, followState = {}, attackState = {}, deadState = {}, State = {}}
 
 
-function clientAIState.idleState.enter(enemy, player)
+function clientAIState.idleState.enter(enemy, playerTarget)
 	enemy.animationState = 1
 end
 
-function clientAIState.idleState.update(enemy, player, dt)
+function clientAIState.idleState.update(enemy, playerTarget, dt)
 	--print("Update")
 end
 
-function clientAIState.idleState.exit(enemy, player)
+function clientAIState.idleState.exit(enemy, playerTarget)
 
 end
 
-function clientAIState.followState.enter(enemy, player)
+function clientAIState.followState.enter(enemy, playerTarget)
 	--print("Client AI Walking")
 	enemy.animationController:doWalk()
 	enemy.animationState = 2
 	--AI.FollowPlayer(player.transformID)
 end
 
-function clientAIState.followState.update(enemy, player, dt)
+function clientAIState.followState.update(enemy, playerTarget, dt)
 	--Transform.Follow(player.transformID, enemy.transformID, enemy.movementSpeed , dt)
 end
 
-function clientAIState.followState.exit(enemy, player)
+function clientAIState.followState.exit(enemy, playerTarget)
 
 end
 
 
 
-function clientAIState.attackState.enter(enemy, player)
+function clientAIState.attackState.enter(enemy, playerTarget)
 	--print("Client AI Attacking")
 
 	enemy.animationController:doAttack()
@@ -41,7 +41,7 @@ function clientAIState.attackState.enter(enemy, player)
 	enemy.actionCountDown = 1.2
 end
 
-function clientAIState.attackState.update(enemy, player, dt)
+function clientAIState.attackState.update(enemy, playerTarget, dt)
 	-- Empty DamagePacket queue and apply the values to player2
 	local newDamageVal, dmg_transformID, dmg_damage = Network.GetDamagePacket()
 	while newDamageVal == true do 
@@ -50,25 +50,25 @@ function clientAIState.attackState.update(enemy, player, dt)
 	end
 end
 
-function clientAIState.attackState.exit(enemy, player)
+function clientAIState.attackState.exit(enemy, playerTarget)
 
 end 
 
-function clientAIState.deadState.enter(enemy, player)
+function clientAIState.deadState.enter(enemy, playerTarget)
 	--print("Client enemy died", enemy.transformID)
 	--enemy.animationController:doNothing()
 
 	enemy:Kill()
 end
 
-function clientAIState.deadState.update(enemy, player)
+function clientAIState.deadState.update(enemy, playerTarget)
 end
 
-function clientAIState.deadState.exit(enemy, player)
+function clientAIState.deadState.exit(enemy, playerTarget)
 
 end 
 
-function setAIState(enemy, player, transformID, aiState)
+function setAIState(enemy, playerTarget, transformID, aiState)
 	--print("Enemy", enemy.transformID)
 	--print("Client AI ID", enemy.transformID)
 	if aiState == 0 then--IdleState
@@ -90,7 +90,7 @@ function setAIState(enemy, player, transformID, aiState)
 		enemy.state = clientAIState.deadState
 	end
 
-	enemy.state.enter(enemy, player)
+	enemy.state.enter(enemy, playerTarget)
 end
 
 baseReturn.setAIState = setAIState
