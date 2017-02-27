14b5a173eb887fc40be581ada5395299468310b1
koolingtuffing <adam@fredrikson.net>
2017-02-27 11:33:24 +0100

    Bugg fixar mannen

--- Erebus/Scripts/enemies.lua
+++ Erebus/Scripts/enemies.lua
@@ -107,7 +107,6 @@ function CreateEnemy(type, position, element)
 				self.health = self.health - damage
 				--print("ID:", self.transformID, "Sending new health:", self.health)
 				Network.SendAIHealthPacket(self.transformID, self.health)
-				print(damage)
 				self.damagedTint.a = 1
 				self.soundID[3] = Sound.Play(SFX_HURT, 1, pos)
 
