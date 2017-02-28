49b352a0072859701cc6faf238e2b1a936d07e53
Taccoa <Mattias_M_93@hotmail.com>
2017-02-28 08:35:05 +0100

    revive

--- Erebus/Scripts/revive.lua
+++ Erebus/Scripts/revive.lua
@@ -25,7 +25,7 @@ function CreateRevive(entity)
 			self.reviveTime = self.reviveTime - dt
 			self.position.y = self.position.y - REVIVE_FALLSPEED * dt
 			Transform.SetPosition(self.transformID, self.position )
-			self.rotation.z = self.rotation.z + dt * 3
+			self.rotation.y = self.rotation.y + dt * 3
 			Transform.SetRotation(self.transformID, self.rotation )
 			Light.updatePos(self.light, self.position.x, self.position.y, self.position.z, true)
 			if self.reviveTime < 0 then 
