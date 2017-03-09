2448ad786dce5cf57cc7b6b10f05c5ccf8dfd1f2
Raxo94 <oskar_swing@hotmail.com>
2017-03-09 11:56:03 +0100

    Lesser cooldown on Charge hellpillar. about twice the damage

--- Erebus/Scripts/hellPillar.lua
+++ Erebus/Scripts/hellPillar.lua
@@ -1,9 +1,9 @@
 --HELLPILLAR_SPELL_TEXTURE = Assets.LoadTexture("Textures/IconHellPiller.dds");
 --BLEND_TERXTURE1 = Assets.LoadTexture("Textures/hellpillarNewTex.dds");
 --BLEND_TERXTURE2 = Assets.LoadTexture("Textures/hellpillarNewTex2.dds");
-MAX_DAMAGE_PILLAR = 8
+MAX_DAMAGE_PILLAR = 14
 MIN_CHARGE_TIME_PILLAR = 1
-COOLDOWN_BIG_PILLAR = 3.5
+COOLDOWN_BIG_PILLAR = 2.2
 HELLPILLAR_CASTSPEED_MULTIPLE = 2.5 + 0.1875
 
 --Divide COOLDOWN_SMALL_PILLAR by 2.5 to get castSpeed for first attack
--- Erebus/Scripts/levelLogic1.lua
+++ Erebus/Scripts/levelLogic1.lua
@@ -48,19 +48,13 @@ function TutorialBarrier(TutorialObject,dt)
 	pos = Transform.GetPosition(TutorialObject.transformID,pos) 
 	showCombineBarrierImage(dt,pos.x,pos.y,pos.z)
 		if player2.position.x==0 then
-			
 			TUTORIAL_DONE = true
 			TUTORIAL_START_ANIM = true
 			TUTORIAL_OBJECT = TutorialObject
 			hideCombinationImage()
 		end
 
-		--local colID = TutorialObject.collider:GetID()
-		--local collisionIDs = TutorialObject.collider:GetCollisionIDs()
-
-		--for i = 1, #collisionIDs do 
-		--	for curID = 1, 3 do
-			--	if player.spells[curID]:GetCollider()[1] == collisionIDs[i] then				
+		
 		if player.isCombined == true or player2.isCombined == true then
 			TUTORIAL_DONE = true
 			TUTORIAL_START_ANIM = true
