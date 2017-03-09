7ae28aded505faab53408415a389148c3b006eb8
jesperico <jespereriksson1992@hotmail.com>
2017-03-09 15:57:52 +0100

    jesperLevel, tile2 stones

--- Erebus/LevelEditorStuff/LevelEditor.cpp
+++ Erebus/LevelEditorStuff/LevelEditor.cpp
@@ -144,7 +144,7 @@ void LevelEditor::start() {
 	engine->queueForwardModels(&forwardInstances);
 	engine->pickActorFromWorld(LevelModelHandler::getInstance()->getModels(), LevelModelHandler::getInstance()->getModelInstanceAgentIDs(), camera, inputs->getMousePos(), actorID, hitPoint, hitNormal);
 
-	LevelActorFactory::getInstance()->loadWorld("Level1Merged8Mars");
+	LevelActorFactory::getInstance()->loadWorld("Level1Jesper2.0");
 	//LevelActorFactory::getInstance()->loadWorld("testLevelXML");
 	//LevelActorFactory::getInstance()->loadWorld("testWorld101");
 	
