275cac2b5a5ae4bd34c8f6cbab6a014df8e4710d
EnemyDawn <maxen940393@gmail.com>
2017-03-10 15:37:38 +0100

    Ai FIX GOD DAMN

--- AGI/AGI.h
+++ AGI/AGI.h
@@ -289,6 +289,7 @@ namespace AGI
 
 		AGI_API void destroyInfluenceMap()
 		{
+			std::cout << "DSDA" << std::endl;
 			for (int n = 0; n < imWidth; n++)
 			{
 				for (int i = 0; i < imHeight; i++)
@@ -693,11 +694,11 @@ namespace AGI
 
 			if (xFrom > 700 || xPlayerPos > 700 || yFrom > 780 || yPlayerPos> 780)
 				return;
+			if (dynamicInfluenceMap == NULL)
+				return;
 
 			if (dynamicInfluenceMap[xPlayerPos][yPlayerPos] == nullptr)
 				return;
-			width = 700;
-			height = 780;
 
 			if (enemies.at(enemyPos).hasTarget())
 			{
