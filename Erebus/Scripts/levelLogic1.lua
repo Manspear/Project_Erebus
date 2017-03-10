4c4e9b7365f35ab01fe17d7ce4f4bee8c736294e
koolingtuffing <adam@fredrikson.net>
2017-03-10 14:02:03 +0100

    Hejhej

--- Erebus/Scripts/levelLogic1.lua
+++ Erebus/Scripts/levelLogic1.lua
@@ -37,9 +37,10 @@ function InteractSpellBook()
 end
 
 function lowerObject(object,time,dt)
+	--local pos = Transform.GetPosition(object.transformID)
 	TUTORIAL_OBJECT.position.y = TUTORIAL_OBJECT.position.y - (dt * 8)
 	TUTORIAL_COUNTER = TUTORIAL_COUNTER + dt
-	Transform.SetPosition(object.transformID, TUTORIAL_OBJECT.position )
+	Transform.SetPosition(object.transformID, TUTORIAL_OBJECT.position)
 	if TUTORIAL_COUNTER > 0.85 then
 		OBBCollider.SetActive(object.collider,false)
 		TUTORIAL_START_ANIM=false
@@ -51,7 +52,7 @@ function TutorialBarrier(TutorialObject,dt)
 	
 	--this is triggered upon both players
 	if TUTORIAL_DONE == false then
-		local pos = Transform.GetPosition(TutorialObject.transformID) 
+		local pos = Transform.GetPosition(TutorialObject.transformID,pos) 
 		showCombineBarrierImage(dt,pos.x,pos.y,pos.z)
 		if player2.position.x==0 then
 			TUTORIAL_DONE = true
@@ -60,12 +61,12 @@ function TutorialBarrier(TutorialObject,dt)
 			TUTORIAL_OBJECT.position = pos
 			hideCombinationImage()
 		end
-
-		
+	
 		if player.isCombined == true or player2.isCombined == true then
 			TUTORIAL_DONE = true
 			TUTORIAL_START_ANIM = true
 			TUTORIAL_OBJECT = TutorialObject
+			TUTORIAL_OBJECT.position = pos
 			hideCombinationImage()
 		end			
 	end
--- Erebus/Scripts/lifeStealEffect.lua
+++ Erebus/Scripts/lifeStealEffect.lua
@@ -14,7 +14,7 @@ function CreateLifeStealEffect(entity, duration)
 		self.interval = self.interval - dt
 		self.duration = self.duration - dt
 		if self.interval < 0 then
-			if(self.owner.health < 100) then
+			if self.owner.health < 100 and self.owner.isAlive then
 				self.owner.health = self.owner.health + self.damage
 				Gear.PrintDamage(self.damage, HEAL, self.owner.position.x, self.owner.position.y+1, self.owner.position.z )
 			end
